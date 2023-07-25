2c6e114eb4df2c902afe51a367cb4535fb373fd3
Sam Ireland <mail@samireland.com>
2023-07-25 23:16:30 +0100

    tqdm requirement

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(
     keywords="nextflow bioinformatics pipeline",
     packages=["flowbio"],
     python_requires="!=2.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
-    install_requires=[]
+    install_requires=["tqdm", "kirjava", "requests"]
 )
