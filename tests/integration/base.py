cffdaf92c08310cd64bc9b6295447e26e6de92bb
Sam Ireland <mail@samireland.com>
2023-07-26 06:33:02 +0100

    Test API setup

--- tests/integration/base.py
+++ tests/integration/base.py
@@ -1,4 +1,5 @@
 import shutil
+import socket
 import subprocess
 from pathlib import Path
 from unittest import TestCase
@@ -7,26 +8,48 @@ class ServerTestCase(TestCase):
     
     @classmethod
     def setUpClass(cls):
-        # Delete anything left over
         cls.sever_location = Path("tests", "test-server")
         cls.tearDownClass()
-
-        # Make location for test server
         cls.sever_location.mkdir(exist_ok=True)
-
-        # Clone repo
         subprocess.run([
             "git", "clone", "https://github.com/goodwright/flow-api.git",
             str(cls.sever_location)
-        ], check=True)
-        subprocess.run(["git", "checkout", "1.0"], cwd=cls.sever_location, check=True)
-
-        # Make locations for stuff
+        ], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(
+            ["git", "checkout", "1.0"], cwd=cls.sever_location, check=True,
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+        subprocess.run(
+            ["pip", "install", "-r", "requirements.txt"],
+            cwd=cls.sever_location, check=True, stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+        subprocess.run(
+            ["python", "manage.py", "migrate"],
+            cwd=cls.sever_location, check=True, stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
         Path("tests", "uploads").mkdir(exist_ok=True)
     
 
     @classmethod
     def tearDownClass(cls):
-        # Delete test server
         shutil.rmtree(cls.sever_location, ignore_errors=True)
         shutil.rmtree(Path("tests", "uploads"), ignore_errors=True)
+    
+
+    def setUp(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(("", 0))
+            self.port = str(s.getsockname()[1])
+        self.process = subprocess.Popen(
+            ["python", "manage.py", "runserver", self.port],
+            cwd=self.sever_location, stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+        self.live_server_url = f"http://localhost:{self.port}/graphql"
+
+
+    def tearDown(self):
+        self.process.terminate()
+        self.process.wait()
