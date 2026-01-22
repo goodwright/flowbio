cbeffbde304bc0446afe4f20f8d4625b711547d7
Martin Husbyn <martin@flow.bio>
2026-01-22 15:37:50 +0000

    fix(queries.py): remove incompatible query params
    A big update to the Flow app removed some query capability for GraphQL. That meant that the sample upload stopped working as the sample query tried to query for attributes that no longer exist.
    
    BREAKING CHANGE: Some sample parameters are no longer queriable.

--- flowbio/queries.py
+++ flowbio/queries.py
@@ -16,13 +16,9 @@ SAMPLE = """query sample($id: ID!) {
     sample(id: $id) {
         id name private created sampleType owner { id name username }
         initialData { id created data { id filename } } organism { id name }
-        source { id name } purificationTarget { id name } project { id name }
-        sourceText purificationTargetText threePrimeAdapterName
-        scientist pi organisation purificationAgent experimentalMethod condition
-        sequencer comments fivePrimeBarcodeSequence threePrimeBarcodeSequence 
-        threePrimeAdapterSequence read1Primer read2Primer rtPrimer
-        umiBarcodeSequence umiSeparator strandedness rnaSelectionMethod
-        geo ena pubmed
+        project { id name }
+        strandedness rnaSelectionMethod
+        pubmed
     }
 }"""
 
